adfa6f0362e73031f620f288e5278e06de3408c9
Markus Wenzl <markus.wenzl@outlook.de>
2022-09-06 19:03:23 +0200

    Project init

---
+++ AvaloniaInside.SampleApp/App.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using AvaloniaInside.SampleApp.ViewModels;
+using AvaloniaInside.SampleApp.Views;
+
+namespace AvaloniaInside.SampleApp
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
